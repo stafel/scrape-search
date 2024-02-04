496129308e0ca11e5141f5cad7adc435847eaef8
stafel <rafael.stauffer@hotmail.de>
2024-02-04 15:25:00 +0100

    fix: extract article id from title link

--- searchscrape/searchscrape/spiders/thealexandrian.py
+++ searchscrape/searchscrape/spiders/thealexandrian.py
@@ -1,6 +1,7 @@
 import scrapy
 from ..items import ThealexandrianArticle
 
+
 class ThealexandrianSpider(scrapy.Spider):
     name = "thealexandrian"
     allowed_domains = ["thealexandrian.net"]
@@ -9,10 +10,14 @@ class ThealexandrianSpider(scrapy.Spider):
     def parse(self, response):
         articles = response.css("div.item")
         for article in articles:
+
+            article_link = article.css("div.itemhead").xpath("h3/a").attrib["href"]
+            article_id = article_link.split("/")[4]
+
             article_obj = ThealexandrianArticle(
-                _id="djaflkjfkjslkfl",
+                _id=article_id,
                 title=article.css("div.itemhead").extract_first().strip(),
-                content=article.css("div.storycontent").extract_first().strip()
+                content=article.css("div.storycontent").extract_first().strip(),
             )
 
             yield article_obj
